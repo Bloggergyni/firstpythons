e2409aa758eb2a9fe7063cab7455874f94afd3b0
Gabriel Creasy <gcreasy.pf@gmail.com>
2019-11-12 15:13:47 -0900

    Changes

--- scratch.py
+++ scratch.py
@@ -114,7 +114,6 @@ def playagain():
     hand = []
     dealer = []
     if playagain == "Yes" or playagain == "yes":
-        score = 0
         deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] * 4
         hash = {
             "J": 10,
